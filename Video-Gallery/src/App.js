20f1eb238f8d209c045e3f958a5f5e7e104c24fc
Jade Cohen <Jade@Jades-MacBook-Pro.local>
2022-02-21 20:22:08 +0200

    carousel added, css updates, testing(WIP)

--- Video-Gallery/src/App.js
+++ Video-Gallery/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React, { useState } from "react";
 import Index from "./components/index";
 
 function App() {
--- Video-Gallery/src/components/header.js
+++ Video-Gallery/src/components/header.js
@@ -1,40 +1,82 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { putVideo, addVideoContent } from "../lib/api";
+import Gallery from "./gallery";
+import "../styled/header.css";
+import ReactPlayer from "react-player";
 
-const Header = () => {
-  const [addVideo, setAddVideo] = useState("");
-  const [video, setVideo] = useState("");
-  //   const handleChange = (event) => {
-  //     const value = event.target.value;
-  //     setAddVideo({
-  //       ...addVideo,
-  //       [event.target.name]: value,
-  //     });
-  //   };
-
-  const uploadVideo = (event) => {
-    event.preventDefault();
-    console.log(addVideo);
-
-    const formData = new FormData();
-    formData.append("video", video);
+const Header = ({ setModalIsOpen, modalIsOpen }) => {
+  const [addVideo, setAddVideo] = useState({
+    id: "",
+    name: "",
+    description: "",
+    video: "",
+  });
+  const [videoUrl, setVideoUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setAddVideo({
+      ...addVideo,
+      [event.target.name]: value,
+    });
   };
+
+  const uploadVideo = async (event) => {
+    event.preventDefault();
+    const videoContent = await addVideoContent(addVideo);
+    setModalIsOpen(false);
+    const id = videoContent;
+    const formData = new FormData();
+    formData.append("video", videoUrl);
+    try {
+      const video = await putVideo(id, formData);
+      addVideo.video = video;
+      addVideo.id = id;
+    } catch (err) {
+      console.log(err);
+    }
+    setAddVideo({
+      id: "",
+      name: "",
+      description: "",
+      video: "",
+    });
+  };
+
   return (
-    <div>
-      header here
+    <div className="headerForm">
       <form onSubmit={uploadVideo}>
         <input
           type="file"
-          //   accept="video/*"
           accept=".mov,.mp4"
           name="video"
-          onChange={(event) => setAddVideo(event.target.files[0])}
-          //   value={addVideo}
+          onChange={(event) => setVideoUrl(event.target.files[0])}
+          required
         />
-        <button className="uploadVideoBtn">UPLOAD VIDEO</button>
+        <input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          value={addVideo.name}
+          placeholder="name your video"
+          required
+        />
+        <input
+          type="text"
+          name="description"
+          onChange={handleChange}
+          value={addVideo.description}
+          placeholder="description"
+          required
+        />
+
+        <button className="uploadVideoBtn" disabled={!videoUrl}>
+          UPLOAD VIDEO
+        </button>
       </form>
-   
     </div>
   );
 };
